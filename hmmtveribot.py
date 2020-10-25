4e74f768ab99807ef454f521b9000700acfbf888
josephhuxley <harrypotter0628@outlook.com>
2020-10-25 19:56:44 +0800

    basic verification done

--- hmmtveribot.py
+++ hmmtveribot.py
@@ -4,6 +4,7 @@ import os
 import pickle
 import discord
 import boto3
+from boto3.dynamodb.conditions import Key
 from discord.ext import commands
 from discord.utils import get
 from dotenv import load_dotenv
@@ -12,25 +13,24 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 #GUILD = os.getenv('DISCORD_GUILD')
 
-dynamodb = boto3.resource('dynamodb')
+dynamodb = boto3.client('dynamodb')
 
-table = dynamodb.Table('Discord')
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to the 2020 HMMT November server! If you are registered for HMMT, you will receive the verified role in the server. If you did not get the role and believe this is a mistake, please DM a moderator!'
-    )
-    response = table.query(
-            KeyConditionExpression='DiscordHandle = :name',
+async def on_message(message):
+    if message.content.startswith("!verify"):
+        await message.channel.send(
+            f'Hi {message.author}, welcome to the 2020 HMMT November server! If you are registered for HMMT, you will receive the verified role in the server. If you did not get the role and believe this is a mistake, please DM a moderator!'
+        )
+        response = dynamodb.query(
+            TableName='Discord',
+            KeyConditionExpression="DiscordHandle = :name",
             ExpressionAttributeValues={
-                ':name': {
-                    'S': member.name
-                    }
-                })
-    await member.dm_channel.send(response['Items'])
+                ":name": {"S": str(message.author)}
+                }
+            )
+        await message.channel.send(response['Items'])
 
 bot.run(TOKEN)
