ee62734e37b15b75dc780d2b6ff5cac37edc4df3
josephhuxley <harrypotter0628@outlook.com>
2020-11-05 17:54:46 +0800

    verify competitors on startup

--- hmmtveribot.py
+++ hmmtveribot.py
@@ -16,11 +16,16 @@ dynamodb = boto3.client('dynamodb')
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-not_verified_users = []
+not_verified_users = set()
 
 
 @bot.event
 async def on_ready():
+    members = bot.get_all_members()
+    for each_member in members:
+        member_roles = list(map(lambda x: x.name, each_member.roles))
+        if list(set(member_roles) & set(['Officer', 'Moderator', 'Competitor', 'Auth Bot', 'Censor Bot'])) == []:
+            not_verified_users.add(each_member)
     recheck_members.start()
 
 
@@ -34,7 +39,7 @@ async def on_member_join(member):
         role = get(member.guild.roles, name='Competitor')
         await member.add_roles(role)
     else:
-        not_verified_users.append(member)
+        not_verified_users.add(member)
         await member.dm_channel.send("We are not able to verify you at this time. Please enter your discord ID on the HMMO website")
 
 
@@ -54,7 +59,7 @@ def verify_member(member):
 
 @tasks.loop(seconds=3600)
 async def recheck_members():
-    if not_verified_users != []:
+    if len(not_verified_users) > 0:
         for each_member in not_verified_users:
             if verify_member(each_member):
                 role = get(each_member.guild.roles, name='Competitor')
